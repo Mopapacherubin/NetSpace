a10ab4bc207330d68b74ac27f7408a26e0b7fa46
cherubin mopapa <ru7che@gmail.com>
2026-07-16 07:34:52 +0100

    feat: add table media migration

---
+++ database/migrations/1783877418177_create_media_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'media'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('post_id')
+      table.string('type')
+      table.integer('order')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
